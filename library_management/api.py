97c481a0540c8755f6fe6d97e949e9614a1dd647
Monika R <monikaravikumar227@gmail.com>
2026-08-28 12:07:28 +0530

    Added the Todo APIs

--- library_management/api.py
+++ library_management/api.py
@@ -6,6 +6,28 @@ def custom_logic(doc, method=None):
 
 
 @frappe.whitelist()
+def get_recent_todos():
+    todos = frappe.get_list(
+        "ToDo",
+        fields=["name", "description", "owner"],
+        order_by="creation desc",
+        limit_page_length=5,
+    )
+
+    for todo in todos:
+        todo["email"] = frappe.db.get_value(
+            "User",
+            todo["owner"],
+            "email"
+        )
+
+    timestamp = frappe.utils.now()
+
+    return {
+        "timestamp": timestamp,
+        "records": todos,
+    }
+@frappe.whitelist()
 def update_articles():
     Article = DocType("Article")
     Author = DocType("Author")
