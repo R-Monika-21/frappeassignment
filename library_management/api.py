425cb296f74f2d40896fff361b8e0f622473a016
Monika R <monikaravikumar227@gmail.com>
2026-09-04 11:18:21 +0530

    Dialog were added

--- library_management/api.py
+++ library_management/api.py
@@ -27,26 +27,36 @@ def get_recent_todos():
         "timestamp": timestamp,
         "records": todos,
     }
-@frappe.whitelist()
-def update_articles():
-    Article = DocType("Article")
-    Author = DocType("Author")
 
-    query = (
-        frappe.qb.from_(Article)
-        .join(Author)
-        .on(Article.author == Author.name)
+@frappe.whitelist()
+def create_library_member(first_name, last_name, email_address):
+    member = frappe.new_doc("Library member")
+    member.first_name = first_name
+    member.last_name = last_name
+    member.email_address = email_address
+    member.save()
+    return member.name
+
+@frappe.whitelist()
+def update_books_with_query_builder():
+    Book = DocType("Book")
+    BookAuthor = DocType("Book Author")
+
+    results = (
+        frappe.qb.from_(Book)
+        .join(BookAuthor)
+        .on(Book.author == BookAuthor.name)
         .select(
-            Article.name,
-            Article.article_name,
-            Article.status,
-            Author.name.as_("author")
+            Book.name,
+            Book.book_title,
+            Book.status,
+            BookAuthor.author_name,
         )
         .limit(5)
+        .run(as_dict=True)
     )
-
-    results = query.run(as_dict=True)
-
-    print(results)
-
+    if results:
+        book = frappe.get_doc("Book", results[0]["name"])
+        book.status = "Borrowed"
+        book.save()
     return results
--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -1,22 +1,102 @@
-// Copyright (c) 2026, Monika R and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Library member", {
+    refresh(frm) {
 
-// frappe.ui.form.on("Library member", {
-// 	refresh(frm) {
+        
+        frm.add_custom_button("Create Membership", () => {
+            frappe.new_doc("Library Membership", {
+                library_member: frm.doc.name
+            });
+        });
 
-// 	},
-// });
-frappe.ui.form.on('Library member', {
- refresh: function(frm) {
- frm.add_custom_button('Create Membership', () => {
- frappe.new_doc('Library Membership', {
- library_member: frm.doc.name
- })
- })
- frm.add_custom_button('Create Transaction', () => {
- frappe.new_doc('Library Transaction', {
- library_member: frm.doc.name
- })
- })
- }
+        
+        frm.add_custom_button("Create Transaction", () => {
+            frappe.new_doc("Library Transaction", {
+                library_member: frm.doc.name
+            });
+        });
+
+        
+        frm.add_custom_button("Create Contact", () => {
+
+            let dialog = new frappe.ui.Dialog({
+                title: "Create Contact",
+
+                fields: [
+                    {
+                        label: "First Name",
+                        fieldname: "first_name",
+                        fieldtype: "Data"
+                    }
+                ],
+
+                primary_action_label: "Create Contact",
+
+                primary_action(values) {
+                    let first_name = values.first_name;
+
+                    dialog.hide();
+
+                    frappe.route_options = {
+                        first_name: first_name
+                    };
+
+                    frappe.new_doc("Contact");
+                }
+            });
+
+            dialog.show();
+        });
+
+        
+        frm.add_custom_button("Create Library Member", () => {
+    let dialog = new frappe.ui.Dialog({
+        title: "Create Library Member",
+
+        fields: [
+            {
+                label: "First Name",
+                fieldname: "first_name",
+                fieldtype: "Data",
+                reqd: 1
+            },
+            {
+                label: "Last Name",
+                fieldname: "last_name",
+                fieldtype: "Data"
+            },
+            {
+                label: "Email Address",
+                fieldname: "email_address",
+                fieldtype: "Data"
+            }
+        ],
+
+        primary_action_label: "Create Member",
+
+        primary_action(values) {
+            frappe.call({
+                method: "library_management.api.create_library_member",
+                args: {
+                    first_name: values.first_name,
+                    last_name: values.last_name,
+                    email_address: values.email_address
+                },
+
+                callback: function(response) {
+                    dialog.hide();
+
+                    frappe.msgprint({
+                        title: "Success",
+                        message: "Library Member " + response.message + " created successfully.",
+                        indicator: "green"
+                    });
+                }
+            });
+        }
+    });
+
+    dialog.show();
 });
+    }
+});
+
