ea7e759a257725bce87107b18fd6cb44bf434b44
Monika R <monikaravikumar227@gmail.com>
2026-09-10 15:09:18 +0530

    Print format is added

--- library_management/library_management/report/book_author_report/book_author_report.js
+++ library_management/library_management/report/book_author_report/book_author_report.js
@@ -1,13 +1,9 @@
-// Copyright (c) 2026, Monika R and contributors
-// For license information, please see license.txt
-
 frappe.query_reports["Book Author Report"] = {
-	filters: [
-		// {
-		// 	"fieldname": "my_filter",
-		// 	"label": __("My Filter"),
-		// 	"fieldtype": "Data",
-		// 	"reqd": 1,
-		// },
-	],
+    filters: [
+        {
+            fieldname: "author_name",
+            label: "Author Name",
+            fieldtype: "Data"
+        }
+    ]
 };
